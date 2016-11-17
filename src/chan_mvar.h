ef5fe8b888ccf52b93f260901bf528a80515d3e2
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-11-16 22:44:30 -0500

    Docs

--- src/chan_mvar.h
+++ src/chan_mvar.h
@@ -1,14 +1,36 @@
 #ifndef __LIBSYNC_CHAN_MVAR_H
 #define __LIBSYNC_CHAN_MVAR_H
 
-// A multi-producer, multi-reader thread-safe FIFO channel implementation based
-// on mvars.
+/**
+ * A multi-producer, multi-reader thread-safe FIFO channel implementation based
+ * on mvars. In other words, `chan_write()` and `chan_read()` functions are
+ * thread safe. If a channel is empty read operation blocks until a value
+ * becomes available. Readers blocked in `chan_read()` read values in FIFO
+ * order.
+ */
 
 typedef struct chan_mvar_ chan_mvar;
 
+/**
+ * Create an empty channel.
+ */
 chan_mvar* chan_mvar_new();
+
+/**
+ * Free a channel. Readers blocked in `chan_read()` will stay blocked forever,
+ * so make sure no thread is blocked.
+ */
 void chan_mvar_free(chan_mvar*);
+
+/**
+ * Write a value to a channel. Does not block.
+ */
 void chan_write(chan_mvar*, void*);
+
+/**
+ * Read a value from a channel. Blocks if the channel is empty. Threads blocked
+ * by this function read values in FIFO order.
+ */
 void* chan_read(chan_mvar*);
 
 #endif
--- src/mvar.h
+++ src/mvar.h
@@ -1,11 +1,36 @@
 #ifndef __LIBSYNC_MVAR_H
 #define __LIBSYNC_MVAR_H
 
+/**
+ * An mvar is a mutable cell with a FIFO queue for multi-threaded use.
+ * Initially it's empty. Writing (`mvar_put()`) to an empty mvar and reading
+ * (`mvar_take()`) a filled mvar do not block. Otherwise threads threads blocks
+ * and continue in FIFO order.
+ */
+
 typedef struct mvar_ mvar;
 
+/**
+ * Create a new empty mvar.
+ */
 mvar* mvar_new();
+
+/**
+ * Free an mvar. Threads blocked by this mvar will stay blocked forever so make
+ * sure no threads are blocked before freeing an mvar.
+ */
 void mvar_free(mvar*);
+
+/**
+ * Write a value to an mvar. If the mvar not empty then this function blocks
+ * until the thread is able to write.
+ */
 void mvar_put(mvar*, void*);
+
+/**
+ * Take value of an mvar. If the mvar is empty then this function blocks until
+ * another thread puts a value in this mvar.
+ */
 void* mvar_take(mvar*);
 
 #ifdef USE_EVENTFD
